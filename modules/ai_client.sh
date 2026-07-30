abc07492411eebe562ba8adcb882b4ba48dc6c32
Srijit-Shirsat <srijitshirsat805@gmail.com>
2026-07-30 15:23:56 +0000

    add network timeout and API error handling

--- modules/ai_client.sh
+++ modules/ai_client.sh
@@ -21,10 +21,33 @@ ask_ai(){
     }
   ]
 }')
-	response=$(curl "$BASE_URL/$MODEL_NAME:$ENDPOINT?key=$GEMINI_API_KEY" \
-    	-H 'Content-Type: application/json' \
-   	-X POST \
-	-d "$request_body" 2> logs/curl_error.log )
+	response=$(curl --connect-timeout 10 \ 
+	        	--max-time 30 \
+			"$BASE_URL/$MODEL_NAME:$ENDPOINT?key=$GEMINI_API_KEY" \
+    			-H 'Content-Type: application/json' \
+   			-X POST \
+			-d "$request_body" 2> logs/curl_error.log )
+
+	curl_status=$?
+
+	if [[ $curl_status -ne 0 ]]; then
+		case $curl_status in
+			6)
+				echo "Unable to resolve host. Please check your internet connection."
+				;;
+			7)
+				echo "Failed to connect to Gemini."
+				;;
+			28)
+				echo "Request timed out."
+				;;
+			*)
+				echo "Network error (curl: $curl_status)"
+				;;
+		esac
+		
+		return 1
+	fi
 
 	if echo "$response" | jq -e '.error' >/dev/null; then
 		echo "Gemini API Error:"
@@ -32,13 +55,8 @@ ask_ai(){
 		return 1
 	fi
 	
-	if [[ $? -eq 0 ]] then
-		answer=$(echo "$response" | jq -r '.candidates[0].content.parts[0].text')
-		echo "$answer" 
-	else
-		echo "Unable to connect to Gemini.
-		      Please check your internet connection."
-	fi
+	answer=$(echo "$response" | jq -r '.candidates[0].content.parts[0].text')
+	echo "$answer"
 
 }
 
